04afb16f2a6fec716903e5542ee32530c4977db9
Anastasia Calico <calico@merchantcalico.net>
2025-08-13 12:44:33 +1000

    feat: Initial setup for main class.

--- src/main/java/net/merchantcalico/fiveguysburgersandfries/FiveGuysBurgersAndFries.java
+++ src/main/java/net/merchantcalico/fiveguysburgersandfries/FiveGuysBurgersAndFries.java
@@ -1,15 +1,21 @@
 package net.merchantcalico.fiveguysburgersandfries;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FiveGuysBurgersAndFries implements ModInitializer {
-	public static final String ID = "mod_id";
-	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+	public static final String MOD_ID = "fiveguysburgersandfries";
+	public static final String MOD_NAME = "5 Guys (Burgers and Fries)";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
 	@Override
 	public void onInitialize() {
-		//LOGGER.info("[Mod ID] pretty pink princess ponies prancing perpendicular");
+
+	}
+
+	public static ResourceLocation id(String path) {
+		return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
 	}
 }
